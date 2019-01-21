e34440afe357cbfc4d6d378e33c7dd8e8091cc0e
Chengmin Zhang <zhangchengmin@macbook-pro-3.local>
2019-01-21 10:26:16 +0800

    初始化项目

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by 张成敏 on 2019/1/21.
+//  Copyright © 2019 Beijing University of Posts and Telecommunications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
